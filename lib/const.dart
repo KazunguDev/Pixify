dbe208e81c279d7b7daf0bb833aaa8f140d2408e
KazunguDev <kazungudev@gmail.com>
2021-08-09 09:16:12 +0300

    Add files via upload

---
+++ lib/const.dart
@@ -0,0 +1,9 @@
+///Get API key from https://www.pexels.com/api/
+final String apiKey = "563492ad6f91700001000001617e2555a84546d8bdd5445c4ef817ec";
+final String editorChoiceEndPoint =
+    "https://api.pexels.com/v1/search?query=wallpaper";
+final String perPageLimit = "&per_page=30";
+final String searchEndPoint = "https://api.pexels.com/v1/search?query=";
+final String message = "Check out this awesome wallpaper app by KazunguDev:";
+final String url = "https://play.google.com/store/apps/details?id=";
+final String gitHubUrl = "https://github.com/KazunguDev";
